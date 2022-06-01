96c2e09c34995408cdcd8466f10e78c2ec97a42d
“YCJhongik” <changcho0618@gmail.com>
2022-06-01 20:47:49 +0900

    과제 3 통합본 YCJ

--- entity.h
+++ entity.h
@@ -19,7 +19,7 @@ private:
 	string clientPassword;
 	int log_status;
 
-	// ProductCollection purchasedProductList; //구매자에서 추가
+	ProductCollection purchasedProductList; //구매자에서 추가
 public:
 	void createClient(string& name, string& SSN, string& ID, string& password);
 	string getClientID();         //어떤 정보 접근할지 몰라서 모든 값들 반환하도록 만들었습니다.
@@ -28,15 +28,15 @@ public:
 	string getClientPW();
 	bool getlog_status();
 	void set_log(int log_status); // 로그인상태를 바꾸는 함수
+	void deleteClient(); //재고 및 판매량 수정
 
-
-	//void addPurchaseInformation(Product* product); //구매자에서 추가
-	//ProductCollection getPurchasedProductList();   //구매자에서 추가
+	void addPurchaseInformation(Product* product); //구매자에서 추가
+	ProductCollection getPurchasedProductList();   //구매자에서 추가
 };
 
 class ClientCollection {
 private:
-	vector<Client*> clients; //일단은 시간이 촉박해서 public으로 놓았는데 private으로 수정예정입니다!
+	vector<Client*> clients; 
 public:
 	int findClientIndex(string ClientName); // 이름을 통해 아이디 인덱스 찾기 // 1.2구현을 위해 사용
 	Client* Login(string ID, string PW);  //로그인 할 아이디의 로그인상태값 변경을 위해 호출 // 2.1로그인에서!
@@ -50,6 +50,48 @@ public:
 
 
 
+class Product {
+private:
+	string sellerID; //추가
+	string productName;
+	string productCompanyName;
+	int cost;
+	int quantityLeft;
+	int quantitysold;
+	float averageScore;
 
+public:
+	/*
+	Product() { //실험용으로 만든 생성자. push 전에 지우기.
+		sellerID = "mbc";
+		productName = "hat";
+		productCompanyName = "chulsoo";
+		cost = 2000;
+		quantityLeft = 2;
+		quantitysold = 0;
+		averageScore = 0.0; //구매만족도 초기값은 0. 이것만 남겨두기.
+	}
+	*/
+	void getProductDetails(string& ID, string& name, string& companyName, int& productCost, int& left);
+	string getProductName();//필요해서 추가함
+	string getName(); //새로 추가한 함수. Search에서 필요함.
+	string getSellerID();
+	void setScore(int score);
+	void stockCorrection();
+	void createProduct() {}; //
+	void getProductCostAndScore() {};//
+
+};
+
+
+class ProductCollection {
+private:
+	vector<Product*> products;
+public:
+	Product* findProduct(string productName);
+	void addProduct(Product* product);
+	int getSize();
+	Product* getProduct(int i);
+};
 
 
